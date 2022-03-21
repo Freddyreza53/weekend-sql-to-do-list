7d288cc1be5ecf8bc3602be0cff2ff50870e01ba
Freddy Reza <rezafreddy@gmail.com>
2022-03-21 09:49:20 -0500

    add dummy data to database.sql file

--- server/database.sql
+++ server/database.sql
@@ -4,3 +4,14 @@ CREATE TABLE "todo_list" (
 	"complete_by_date" DATE NOT NULL,
 	"status" BOOLEAN DEFAULT FALSE
 );
+
+-- Dummy Data
+INSERT INTO "todo_list" 
+	("task", "complete_by_date") 
+VALUES 
+	('Go grocery shopping', '01-01-2023'),
+	('Take trash out', '01-01-2023'),
+	('Finish Assignment', '02-10-2023'),
+	('Grade assignment', '03-21-2023'),
+	('Go to the gym', '04-15-2023'),
+	('Pick up dog', '04-24-2023');
